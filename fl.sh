83d42f00919483c57ebdf829074b891933906bec
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-10-01 17:10:22 +0200

    minor edits

---
+++ fl.sh
@@ -0,0 +1,7 @@
+conda create -y -n fl python=3 jupyter notebook
+conda activate fl
+conda install -y -c anaconda ply
+conda install -y -c conda-forge antlr4-python3-runtime
+conda install -y -c conda-forge antlr
+
+
