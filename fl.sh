4de3c9dd387b883b4468b02204d8855b536a92d9
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-10-14 19:37:57 +0200

    minor edits

--- fl.sh
+++ fl.sh
@@ -3,5 +3,6 @@ conda activate fl
 conda install -y -c anaconda ply
 conda install -y -c conda-forge antlr4-python3-runtime
 conda install -y -c conda-forge antlr
-
+conda install -y -c anaconda graphviz
+conda install -y -c conda-forge python-graphviz
 
