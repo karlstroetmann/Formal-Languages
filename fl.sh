45313b225e810eeb818db26b3e6f9e4bdd0c5a38
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-11-01 13:15:52 +0100

    translation

--- fl.sh
+++ fl.sh
@@ -1,4 +1,4 @@
-conda create -y -n fl python=3 jupyter notebook
+conda create -y -n fl python=3.9 jupyter notebook
 conda activate fl
 conda install -y -c anaconda ply
 conda install -y -c conda-forge antlr4-python3-runtime
