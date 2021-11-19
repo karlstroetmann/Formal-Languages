4273c2d289a058d53eed177feea3222326dba82e
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-11-19 17:46:48 +0100

    Interpreter-Matching.ipynb added

--- fl.sh
+++ fl.sh
@@ -1,11 +1,11 @@
 # The first group of commands can be used to install Python 3.10. This version supports matching.
 conda create -n fl
 conda activate fl
-conda install -c conda-forge python=3.10 jupyter notebook
+conda install -y -c conda-forge python=3.10 jupyter notebook
 conda install -y -c conda-forge ply
 conda install -y -c conda-forge antlr4-python3-runtime
 conda install -y -c conda-forge antlr
-conda install -y -c anaconda graphviz
+conda install -y -c conda-forge graphviz
 conda install -y -c conda-forge python-graphviz
 
 # This group of commands installs python 3.9.
