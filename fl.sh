f339457584e41f6e418dc8a4894862cdfea7aa1f
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2023-11-06 20:28:40 +0100

    EBNF-Parser moved

--- fl.sh
+++ fl.sh
@@ -1,19 +1,10 @@
 # The first group of commands can be used to install Python 3.10. This version supports matching.
 conda create -n fl
 conda activate fl
-conda install -y -c conda-forge python=3.10 jupyter notebook
+conda install -y -c conda-forge python=3.11 jupyter notebook
 conda install -y -c conda-forge ply
 conda install -y -c conda-forge antlr4-python3-runtime
 conda install -y -c conda-forge antlr
 conda install -y -c conda-forge graphviz
-conda install -y -c conda-forge python-graphviz
-
-# This group of commands installs python 3.9.
-conda create -y -n fl python=3.9 jupyter notebook
-conda activate fl
-conda install -y -c anaconda ply
-conda install -y -c conda-forge antlr4-python3-runtime
-conda install -y -c conda-forge antlr
-conda install -y -c anaconda graphviz
 conda install -y -c conda-forge python-graphviz
 
