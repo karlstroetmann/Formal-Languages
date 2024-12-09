ccfc80aa1c0e705fbeac618fe7d9d5e53997a60b
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2024-12-09 11:54:42 +0100

    PrimesFor2.c added

---
+++ Python/Chapter-12/Examples/PrimesFor2.c
@@ -0,0 +1,23 @@
+/*** This function checks whether n is a prime number.
+     It returns the number n, if n is prime.  Otherwise,
+     0 is returned.
+ ***/
+int is_prime(int n) {
+    int f;
+    for (f = 2; f < n; f = f + 1) {
+        if (n % f == 0)
+            return 0;
+    }
+    return n;
+}
+// This function computes all primes less than 100.
+int main() {
+    int n;
+    int p;
+    for (n = 2; n < 100; n = n + 1) {
+        p = is_prime(n);
+        if (p > 0) {
+            println(p);
+        }
+    }
+}
