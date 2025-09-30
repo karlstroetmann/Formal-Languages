47e4a8f582c1474232771988c005da975eb6b43a
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2025-09-30 14:11:11 +0200

    Switched font to Atkinson Hyperlegible

--- fl.sh
+++ fl.sh
@@ -1,9 +1,11 @@
 conda create -n fl
 conda activate fl
-conda install -y -c conda-forge python=3.12 jupyter notebook
+conda install -y python=3.13 jupyter nbclassic
 pip install ply
 conda install -y -c conda-forge graphviz
 conda install -y -c conda-forge python-graphviz
 pip install mypy
 pip install nb-mypy
+pip install ipynb
+
 
